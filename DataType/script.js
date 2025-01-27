27eccf33013867de607f2c29a6a0357563cb55e5
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-01-27 15:15:39 +0530

    Output based question on Type Conversion

---
+++ DataType/script.js
@@ -0,0 +1,10 @@
+// some output based questions
+console.log(null == undefined); // true
+console.log(null == null); // true
+console.log(null == 0); //false
+console.log(undefined == undefined); // true
+console.log(undefined == 0); // false
+console.log(NaN == 0); // false
+console.log(NaN == NaN); // false
+console.log(undefined == NaN); // false
+console.log(null == NaN); // false
