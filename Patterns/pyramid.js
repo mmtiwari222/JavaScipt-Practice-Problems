27ed8b79e3c67e3cc2e3cb54de2790905b45b480
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-03-27 22:56:47 +0530

    Pyramid pattern in JS

---
+++ Patterns/pyramid.js
@@ -0,0 +1,17 @@
+let rows = 5; // Number of rows
+
+for (let i = 1; i <= rows; i++) {
+  let str = "";
+
+  // Print spaces
+  for (let j = 1; j <= rows - i; j++) {
+    str += " ";
+  }
+
+  // Print stars
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    str += "*";
+  }
+
+  console.log(str);
+}
